d5433687bcabc504366a2e56f84f4104d7496b06
Bruno <brunofirmino960@gmail.com>
2026-04-26 19:47:32 -0300

    conectando ao banco

---
+++ src/services/supabase.js
@@ -0,0 +1,10 @@
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from '@env'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
