5c552a640a9540b02377cd6a4f4ecc26e07339a1
Bruno <brunofirmino960@gmail.com>
2026-04-26 21:39:33 -0300

    conexão com o banco corrigida

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,36 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
+import { supabase } from "../src/services/supabase";
 
-export default function Index() {
+export default function Home() {
+
+  useEffect(() => {
+    async function test() {
+      console.log("🔍 Iniciando teste...");
+
+      // 🔎 TESTE 1 - SELECT
+      const { data: selectData, error: selectError } = await supabase
+        .from("test")
+        .select("*");
+
+      console.log("SELECT DATA:", selectData);
+      console.log("SELECT ERROR:", selectError);
+
+      // 🧪 TESTE 2 - INSERT
+      const { data: insertData, error: insertError } = await supabase
+        .from("test")
+        .insert([{ name: "Bruno" }]);
+
+      console.log("INSERT DATA:", insertData);
+      console.log("INSERT ERROR:", insertError);
+    }
+
+    test();
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>Testando Supabase...</Text>
     </View>
   );
 }
